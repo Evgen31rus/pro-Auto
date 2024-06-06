a6d8dd5e6fd26921018916f7699bc38e1fc80db5
Evgeniy Platnikov <127827144+Evgen31rus@users.noreply.github.com>
2024-06-06 21:23:10 +0300

    https://github.com/Evgen31rus/pro-Auto.git

--- src/components/FormDiagnosticEnd.tsx
+++ src/components/FormDiagnosticEnd.tsx
@@ -9,15 +9,15 @@ export default function FormDiagnosticEnd(){
     const trigger = useSelector((state:RootState)=> state.sliseForm.formServies.name!==null)
     return(
         <CSSTransition in={trigger} timeout={300} mountOnEnter unmountOnExit classNames={`alert`}>
-        <div className={`w-[100%] min-h-[500px] bg-[#EDEDED] flex flex-col justify-around items-center`}>
+        <div className={`w-[100%] min-h-[500px] bg-[#EDEDED] flex flex-col justify-around items-center sm:mt-10`}>
             <div className={`w-[200px] h-[200px] bg-center bg-cover`}
             style={{
                 backgroundImage: `url('${png}')`
             }}
             >
             </div>
-            <p className={`text-[1.2rem]`}>Ваша заявка успешна отправленна, скоро с вами свяжется наш специолист!</p>
-<Link to={`/`} className={`w-[15%] h-[40px] `}><MainButton textContent={`НА ГЛАВНУЮ`} textColor={`#C53720`}/></Link>
+            <p className={`text-[1.2rem] sm:text-center`}>Ваша заявка успешна отправлена, скоро с вами свяжется наш специaлист!</p>
+<Link to={`/`} className={`w-[15%] h-[40px] sm:w-[50%]`}><MainButton textContent={`НА ГЛАВНУЮ`} textColor={`#C53720`}/></Link>
         </div>
         </CSSTransition>
     )
