a67ef3843da40a42a53e55a52ab096e67a509021
Evgeniy Platnikov <127827144+Evgen31rus@users.noreply.github.com>
2024-06-09 15:05:54 +0300

    https://github.com/Evgen31rus/pro-Auto.git

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,6 +6,7 @@ import png404 from'../../img/404Page/404.png'
 import Nav from "./navBar/Nav"
 import { useLocation } from 'react-router-dom';
 
+
 export default function Header(){
     const location = useLocation()
     console.log(location)
@@ -13,17 +14,13 @@ export default function Header(){
         <div className={`flex flex-col relative items-center w-[100%] max-w-[1900px] h-[600px] bg-cover  bg-no-repeat sm:hidden
         ${location.key!="default"? 'h-[600px]' : 'h-[90vh]'} `}
         style={{
-            backgroundImage: `url('${location.key!="default"||location.pathname=='/pro-Auto/'? MainBanner : Banner404}')`
+            backgroundImage: `url(${MainBanner})`
         }}>
         <Nav />
     
         <div className={`w-[100%] max-w-[1300px] h-[100%] relative`}>
-            {
-                location.key!="default"||location.pathname=='/pro-Auto/'? 
                 <Banner  bannerButtonContent={`ПЕРЕЙТИ В КАТАЛОГ`} link={'/Каталог'} bannerContent={`НАДЕЖНЫЕ, КАЧЕСТВЕННЫЕ АККУМУЛЯТОРЫ`}/>
-                :
-                <Banner error={true} png={png404} bannerButtonContent={`НА ГЛАВНУЮ`} link={`/`} bannerContent={`СТРАНИЦА НЕ НАЙДЕНА`} info={`Извините, но такой страницы нет на нашем сайте. Возможно вы ввели неправильный адрес.`}/>
-            }
+              
         
         </div>
         </div>
